29fcaec27890599542f7db608936e24f8eb02aa2
black801 <81705085+black801@users.noreply.github.com>
2021-05-11 17:18:34 +0200

    Add files via upload

--- DRAGON.lua
+++ DRAGON.lua
@@ -157,7 +157,7 @@ io.popen("mkdir File_Bot")
 io.popen("cd File_Bot && rm -rf commands.lua.1") 
 io.popen("cd File_Bot && rm -rf commands.lua.2") 
 io.popen("cd File_Bot && rm -rf commands.lua.3") 
-io.popen("cd File_Bot && wget https://raw.githubusercontent.com/black801/memo/main/File_Bot/commands.lua") 
+io.popen("cd File_Bot && wget https://raw.githubusercontent.com/black801/Bak/main/File_Bot/commands.lua") 
 t = "\27[35m".."\nAll Files Started : \n____________________\n"..'\27[m'
 i = 0
 for v in io.popen('ls File_Bot'):lines() do
@@ -1083,7 +1083,7 @@ end
 
 if text == 'تحديث السورس ' and DevSoFi(msg) then 
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/black801/memo/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/black801/Bak/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' 🦅 تم تحديث السورس \n 🦅 لديك اخر اصدار لسورس بكار\n 🦅 الاصدار » { v 1.5}')
 dofile('DRAGON.lua')  
 end
@@ -2378,7 +2378,7 @@ end
 return false
 end
 os.execute('rm -rf DRAGON.lua')
-os.execute('wget https://raw.githubusercontent.com/black801/memo/main/DRAGON.lua')
+os.execute('wget https://raw.githubusercontent.com/black801/Bak/main/DRAGON.lua')
 send(msg.chat_id_, msg.id_,' 🦅 تم تحديث السورس \n 🦅 لديك اخر اصدار لسورس بكار\n 🦅 الاصدار » { v 1.5}')
 dofile('DRAGON.lua')  
 end
@@ -2928,7 +2928,7 @@ end
 
 if text == 'العاب بكار' then
 local Text = [[
-𝒔𝒐𝒖𝒓𝒄𝒆 𝒃𝒂𝒌𝒂𝒓
+??𝒐𝒖𝒓𝒄𝒆 𝒃𝒂𝒌𝒂𝒓
 ]]
 keyboard = {} keyboard.inline_keyboard = {
 {{text = 'فلابي بيرد', url="https://t.me/awesomebot?game=FlappyBird"},{text = 'تحداني فالرياضيات',url="https://t.me/gamebot?game=MathBattle"}},   
@@ -4203,7 +4203,7 @@ send(msg.chat_id_, msg.id_,t)
 end
 if text == "متجر الملفات" or text == 'المتجر' then
 if DevSoFi(msg) then
-local Get_Files, res = https.request("https://raw.githubusercontent.com/black801/memo/main/getfile.json")
+local Get_Files, res = https.request("https://raw.githubusercontent.com/black801/Bak/main/getfile.json")
 if res == 200 then
 local Get_info, res = pcall(JSON.decode,Get_Files);
 vardump(res.plugins_)
@@ -4241,7 +4241,7 @@ t = " 🦅 الملف » "..file.."\n 🦅 تم تعطيل ملف \n"
 else
 t = " 🦅 بالتاكيد تم تعطيل ملف → "..file.."\n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/black801/memo/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/black801/Bak/main/File_Bot/"..file)
 if res == 200 then
 os.execute("rm -fr File_Bot/"..file)
 send(msg.chat_id_, msg.id_,t) 
@@ -4261,7 +4261,7 @@ t = " 🦅 بالتاكيد تم تفعيل ملف → "..file.." \n"
 else
 t = " 🦅 الملف » "..file.."\n 🦅 تم تفعيل ملف \n"
 end
-local json_file, res = https.request("https://raw.githubusercontent.com/black801/memo/main/File_Bot/"..file)
+local json_file, res = https.request("https://raw.githubusercontent.com/black801/Bak/main/File_Bot/"..file)
 if res == 200 then
 local chek = io.open("File_Bot/"..file,'w+')
 chek:write(json_file)
